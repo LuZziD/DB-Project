6d084fe6c97f0304a0e771763a65fd1e6aa1c970
LuZziD <ziebartjevgeni@googlemail.com>
2015-01-11 18:17:08 +0100

    Datatable

--- php/sortiment.php
+++ php/sortiment.php
@@ -9,7 +9,7 @@
         <title>Hauptseite</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="layout-sortimentphp.css" type="text/css">
-        <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.4/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" src="datatable.css">
         <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
         <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
     <script type="text/javascript">
@@ -36,8 +36,6 @@
         </div>
         <div id="container">
             <div id="content">
-            <h2 align="left">Unser Sortiment</h2>
-            </div>
             <div id="table">
                 <?php 
                     $sql=mysql_query ( '
@@ -70,9 +68,9 @@
 				<th>Genre</th>
 				<th>Dauer</th>
 				<th>Publisher</th>
-				<th>Altersbeschränkung</th>
-				<th>SonDatumsDing</th>
-				<th>NochSonDatumsDing</th>
+				<th>FSK</th>
+				<th>Release</th>
+				<th>Publishing</th>
 				<th>Menge</th>
 				<th>Mietpreis</th>
 				<th>Kaufpreis</th>
@@ -86,9 +84,9 @@
 				<th>Genre</th>
 				<th>Dauer</th>
 				<th>Publisher</th>
-				<th>Altersbeschränkung</th>
-				<th>SonDatumsDing</th>
-				<th>NochSonDatumsDing</th>
+				<th>FSK</th>
+				<th>Release</th>
+				<th>Publishing</th>
 				<th>Menge</th>
 				<th>Mietpreis</th>
 				<th>Kaufpreis</th>
@@ -96,6 +94,7 @@
 				</tfoot>
 				<tbody>"; while($row=mysql_fetch_array($sql)) { echo "<tr>"; echo "<td>" . $row[ 'article_title'] . "</td>"; echo "<td>" . $row[ 'director_firstname'] . "</td>"; echo "<td>" . $row[ 'genre_name'] . "</td>"; echo "<td>" . $row[ 'article_runtime'] . "</td>"; echo "<td>" . $row[ 'publisher_name'] . "</td>"; echo "<td>" . $row[ 'agelimit_name'] . "</td>"; echo "<td>" . $row[ 'article_releasedate'] . "</td>"; echo "<td>" . $row[ 'article_publishingdate'] . "</td>"; echo "<td>" . $row[ 'article_amount'] . "</td>"; echo "<td>" . $row[ 'article_rentprice'] . "</td>"; echo "<td>" . $row[ 'article_purchaseprice'] . "</td>"; echo "</tr>"; } echo "</tbody>"; echo "</table>"; ?>
             </div>
+			</div>
         </div>
     </div>
 
